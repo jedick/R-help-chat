c5c113e094b021bbf67a2bdaa32da84ca869e4f8
jedick <j3ffdick@gmail.com>
2025-07-12 18:59:04 +0800

    Add conversational app

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.documents import Document
-from langgraph.graph import START, END, StateGraph
+from langchain_core.messages import SystemMessage
+from langchain_core.tools import tool
+from langgraph.graph import START, END, MessagesState, StateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.checkpoint.memory import MemorySaver
 from typing_extensions import List, Annotated, TypedDict
 from transformers import AutoTokenizer
 from dotenv import load_dotenv
@@ -122,16 +126,6 @@ def ProcessDirectory(path):
             print(f"Added embeddings for {file_path}")
         else:
             print(f"No change for {file_path}")
-
-
-def ListDocuments():
-
-    # Get retriever instance
-    retriever = BuildRetriever("dense", embedding_type)
-    # Retrieve all document IDs
-    document_ids = retriever.vectorstore.get()["ids"]
-    # Return the document IDs
-    return document_ids
 
 
 def GetChatModel(chat_type):
@@ -265,3 +259,185 @@ def RunGraph(query, search_type: str = "hybrid_rr", chat_type=chat_type):
     # print(f'Response: {result["response"]}')
     result = graph.invoke({"question": query})
     return result["response"]
+
+
+def BuildChatGraph(search_type: str = "hybrid_rr", chat_type=chat_type):
+    """
+    Build graph for chat (conversational RAG with memory)
+
+    Args:
+        search_type: Type of search to use. Options: "dense", "sparse", "sparse_rr", "hybrid", "hybrid_rr"
+        chat_type: Type of chat API (remote or local)
+
+    Example: RunGraph("What R functions are discussed?")
+
+    Based on:
+        https://python.langchain.com/docs/tutorials/qa_chat_history
+        https://python.langchain.com/docs/how_to/qa_sources
+    """
+
+    # For tracing
+    # os.environ["LANGSMITH_TRACING"] = "true"
+    # os.environ["LANGSMITH_PROJECT"] = "R-help-chat"
+    # For LANGCHAIN_API_KEY
+    # load_dotenv(dotenv_path=".env", override=True)
+
+    # Get chat model, used in both respond_or_retrieve and generate
+    chat_model = GetChatModel(chat_type)
+
+    # Define start of system message, used in both respond_or_retrieve and generate
+    system_message_prefix = (
+        "You are an assistant that answers questions about R programming. "
+        "Do not respond with your own knowledge or ask the user for more information. "
+        "Instead, use a tool to retrieve information related to the query from the R-help mailing list archives. "
+        "Use only the retrieved information to provide a helpful answer. "
+        "If the retrieved information is insufficient to answer the question, say so. "
+    )
+
+    # Define retrieval tool
+    # We propagate the retrieved documents as artifacts on the tool messages.
+    # That makes it easy to pluck out the retrieved documents.
+    # Below, we add them as an additional key in the state, for convenience.
+    # Define the response format of the tool as "content_and_artifact":
+    @tool(response_format="content_and_artifact")
+    def retrieve(query: str):
+        """Retrieve information related to a query from the R-help mailing list archives"""
+        # Get retriever instance
+        retriever = BuildRetriever(search_type, embedding_type)
+        retrieved_docs = retriever.invoke(query)
+        serialized = "\n\n".join(
+            (f"Source: {doc.metadata}\n" f"Content: {doc.page_content}")
+            for doc in retrieved_docs
+        )
+        return serialized, retrieved_docs
+
+    # Define state for application
+    # Represent the state of our RAG application using a sequence of messages to enable:
+    #   - Tool-calling features of chat models
+    #   - A "back-and-forth" conversational user experience
+    # We will have:
+    #   - User input as a HumanMessage
+    #   - Vector store query as an AIMessage with tool calls
+    #   - Retrieved documents as a ToolMessage.
+    #   - Final response as a AIMessage
+    # Leveraging tool-calling to interact with a retrieval step allows a model to rewrite user queries into more effective search queries
+    class ChatMessagesState(MessagesState):
+        # Add a context key to the state to store retrieved documents
+        context: List[Document]
+
+    # Define response or retrieval step (entry point)
+    # NOTE: This has to be ChatMessagesState, not MessagesState, to access step["context"]
+    def respond_or_retrieve(state: ChatMessagesState):
+        """Generate AI response or tool call for retrieval"""
+        chat_model_with_tools = chat_model.bind_tools([retrieve])
+        # response = chat_model_with_tools.invoke(state["messages"])
+        response = chat_model_with_tools.invoke(
+            [SystemMessage(system_message_prefix)] + state["messages"]
+        )
+        # MessagesState appends messages to state instead of overwriting
+        return {"messages": [response]}
+
+    # Define retrieval step
+    tools = ToolNode([retrieve])
+
+    # Define generation step
+    def generate(state: MessagesState):
+        """Generate a response using the retrieved content"""
+        # Get generated ToolMessages
+        recent_tool_messages = []
+        for message in reversed(state["messages"]):
+            if message.type == "tool":
+                recent_tool_messages.append(message)
+            else:
+                break
+        tool_messages = recent_tool_messages[::-1]
+
+        # Format into prompt
+        docs_content = "\n\n".join(doc.content for doc in tool_messages)
+        system_message_with_context = (
+            f"{system_message_prefix}"
+            "\n\n"
+            "### Retrieved Information:"
+            f"{docs_content}"
+        )
+        conversation_messages = [
+            message
+            for message in state["messages"]
+            if message.type in ("human", "system")
+            or (message.type == "ai" and not message.tool_calls)
+        ]
+        prompt = [SystemMessage(system_message_with_context)] + conversation_messages
+
+        # Run the model
+        response = chat_model.invoke(prompt)
+        context = []
+        # Pluck out the retrieved documents and populate them in the state
+        for tool_message in tool_messages:
+            context.extend(tool_message.artifact)
+        return {"messages": [response], "context": context}
+
+    # Initialize a graph object
+    graph_builder = StateGraph(MessagesState)
+    # Add nodes
+    # A node that fields the user input, either responding directly or using a tool
+    graph_builder.add_node(respond_or_retrieve)
+    # A node for the retriever tool that executes the retrieval step
+    graph_builder.add_node(tools)
+    # A node that generates the final response using the retrieved context
+    graph_builder.add_node(generate)
+    # Set entry point
+    graph_builder.set_entry_point("respond_or_retrieve")
+    # Add edges
+    graph_builder.add_conditional_edges(
+        "respond_or_retrieve",
+        tools_condition,
+        # The first respond_or_retrieve step can respond directly to the user or generate a tool call
+        {END: END, "tools": "tools"},
+    )
+    graph_builder.add_edge("tools", "generate")
+    graph_builder.add_edge("generate", END)
+
+    return graph_builder
+
+
+def RunChat(query: str, thread_id=None):
+    """Run chat with graph for conversational RAG
+
+    Args:
+        query: User query to start the chat
+
+    Example;
+        RunChat("Help with parsing REST API response.")
+    """
+
+    # Build the graph
+    graph_builder = BuildChatGraph()
+
+    # FIXME: Use thread id for memory if given
+    # TypeError: Type is not msgpack serializable: ToolMessage
+    # https://github.com/langchain-ai/langgraph/issues/5054
+    # https://github.com/langchain-ai/langgraph/pull/5115
+    if thread_id is None:
+        graph = graph_builder.compile()
+        config = None
+    else:
+        # FIXME: TypeError: Type is not msgpack serializable: ToolMessage
+        # https://github.com/langchain-ai/langgraph/issues/5054
+        # https://github.com/langchain-ai/langgraph/pull/5115
+        # Compile our application with an in-memory checkpointer
+        memory = MemorySaver()
+        graph = graph_builder.compile(checkpointer=memory)
+        # Specify an ID for the thread
+        config = {"configurable": {"thread_id": thread_id}}
+
+    # When executing a search, we can stream the steps to observe the query generation, retrieval, and answer generation:
+    for step in graph.stream(
+        {"messages": [{"role": "user", "content": query}]},
+        stream_mode="values",
+        config=config,
+    ):
+        step["messages"][-1].pretty_print()
+
+    # We see that the retrieved Document objects are accessible from the application state.
+    # step["context"]
+    return step
