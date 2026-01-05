a94e9360455731f229594b8deb4f0ee2ffc5e116
jedick <j3ffdick@gmail.com>
2026-01-05 19:28:14 +0800

    Use test functions for pytest

--- test_main.py
+++ test_main.py
@@ -8,25 +8,30 @@ load_dotenv(dotenv_path=".env", override=True)
 email_dir = "test_emails/R-help/"
 db_dir = "test_db"
 
-# Create the test database
-ProcessCollection(email_dir, db_dir)
 
-# Define the collection (last part of the email directory path)
-collection = "R-help"
+def test_main():
 
-# Run a query with the chain workflow
-result = RunChain("What R functions are discussed?", db_dir, collection)
-# We should get at least one of these
-assert (
-    "aggregate" in result
-    or "t.test" in result
-    or "lme" in result
-    or "ifelse" in result
-    or "xyplot" in result
-)
+    # Create the test database
+    ProcessCollection(email_dir, db_dir)
 
-# Run a query with the graph workflow
-result = RunGraph(
-    "What dataset was used in a question about plotting with nlme?", db_dir, collection
-)
-assert "BodyWeight" in result["answer"]
+    # Define the collection (last part of the email directory path)
+    collection = "R-help"
+
+    # Run a query with the chain workflow
+    result = RunChain("What R functions are discussed?", db_dir, collection)
+    # We should get at least one of these
+    assert (
+        "aggregate" in result
+        or "t.test" in result
+        or "lme" in result
+        or "ifelse" in result
+        or "xyplot" in result
+    )
+
+    # Run a query with the graph workflow
+    result = RunGraph(
+        "What dataset was used in a question about plotting with nlme?",
+        db_dir,
+        collection,
+    )
+    assert "BodyWeight" in result["answer"]
--- test_retriever.py
+++ test_retriever.py
@@ -11,47 +11,65 @@ load_dotenv(dotenv_path=".env", override=True)
 email_dir = "test_emails/R-devel/"
 db_dir = "test_db"
 
-# Create the test database
-ProcessCollection(email_dir, db_dir)
 
-# Get a dense retriever instance
-retriever = BuildRetriever(
-    db_dir, "R-help", "dense", top_k=1, start_year=2025, end_year=2025
-)
-# The result is a semantically similar match to the query
-results = retriever.invoke("inscrutable")
-assert "anyone who might know enough to actually do it" in results[0].page_content
-# But we don't get an exact match
-assert not "inscrutable" in results[0].page_content
+def test_retriever():
 
-# Try keyword retrieval
-retriever = BuildRetriever(
-    db_dir, "R-help", "sparse", top_k=1, start_year=2025, end_year=2025
-)
-results = retriever.invoke("inscrutable")
-# This time we get an exact match
-assert "inscrutable" in results[0].page_content
+    # Create the test database
+    ProcessCollection(email_dir, db_dir)
 
-# R-devel with hybrid search
-retriever = BuildRetriever(
-    db_dir, "R-devel", "hybrid", top_k=1, start_year=2025, end_year=2025
-)
-results = retriever.invoke("MCMC")
-assert "MCMC" in results[0].page_content
+    # Get a dense retriever instance
+    retriever = BuildRetriever(
+        db_dir, "R-help", "dense", top_k=1, start_year=2025, end_year=2025
+    )
+    # The result is a semantically similar match to the query
+    results = retriever.invoke("inscrutable")
+    assert (
+        "anyone who might know enough to actually do it" in results[0].page_content
+        or "makes no sense" in results[0].page_content
+    )
+    # But we don't get an exact match
+    assert not "inscrutable" in results[0].page_content
 
-# Search by month - sparse
-retriever = BuildRetriever(
-    db_dir, "R-help", "sparse", top_k=6, start_year=2025, end_year=2025, months=["Dec"]
-)
-results = retriever.invoke("the")
-# Check that the source file name for each result contains "December"
-assert all(["December" in result.metadata["source"] for result in results])
+    # Try keyword retrieval
+    retriever = BuildRetriever(
+        db_dir, "R-help", "sparse", top_k=1, start_year=2025, end_year=2025
+    )
+    results = retriever.invoke("inscrutable")
+    # This time we get an exact match
+    assert "inscrutable" in results[0].page_content
 
-# Search by month - dense
-retriever = BuildRetriever(
-    db_dir, "R-help", "dense", top_k=6, start_year=2025, end_year=2025, months=["Oct"]
-)
-results = retriever.invoke("plotting")
-assert all(["October" in result.metadata["source"] for result in results])
-# In the test database, only one email in October 2025 has the word "plot"
-assert "plot" in results[0].page_content
+    # R-devel with hybrid search
+    retriever = BuildRetriever(
+        db_dir, "R-devel", "hybrid", top_k=1, start_year=2025, end_year=2025
+    )
+    results = retriever.invoke("MCMC")
+    assert "MCMC" in results[0].page_content
+
+    # Search by month - sparse
+    retriever = BuildRetriever(
+        db_dir,
+        "R-help",
+        "sparse",
+        top_k=6,
+        start_year=2025,
+        end_year=2025,
+        months=["Dec"],
+    )
+    results = retriever.invoke("the")
+    # Check that the source file name for each result contains "December"
+    assert all(["December" in result.metadata["source"] for result in results])
+
+    # Search by month - dense
+    retriever = BuildRetriever(
+        db_dir,
+        "R-help",
+        "dense",
+        top_k=6,
+        start_year=2025,
+        end_year=2025,
+        months=["Oct"],
+    )
+    results = retriever.invoke("plotting")
+    assert all(["October" in result.metadata["source"] for result in results])
+    # In the test database, only one email in October 2025 has the word "plot"
+    assert "plot" in results[0].page_content
